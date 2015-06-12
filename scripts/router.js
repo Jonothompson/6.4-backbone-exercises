9c24485358ef47958ea9d68ddfe5ed8013d0a63e
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-12 14:16:07 -0400

    got a landing page that will take you to a,b,c,d,e respectivley, and (a) is still posting

--- scripts/router.js
+++ scripts/router.js
@@ -1,18 +1,28 @@
+import IndexView from './views/index';
 import AformView from './views/aformview';
+
 
 import {AformCollection} from './models/aformmodel';
 
 var Router = Backbone.Router.extend({
 	routes: {
-		'': 'aform'
+		'': 'index',
+		'a': 'aform'
 	},
 	
 	initialize: function(){
-		 var aformCollection = new AformCollection();
+		var aformCollection = new AformCollection();
 		this.aformView = new AformView({collection: aformCollection});
 	},
 	
+	index: function() {
+		var index = new IndexView();
+		console.log('index');
+		$('#app').html(index.el);	
+	},
+	
 	aform: function() {
+		console.log('aForm');
 		$('#app').html(this.aformView.el);	
 	},
 	
